921ed22a585f693f0d21851a299fab3baeb425d9
17hao <sqh1107@gmail.com>
2021-01-07 16:34:08 +0800

    CharSet.md

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import Calculus from './CalculusNote';
+import CharSet from './CharSet'
 import LinearAlgMd from './LinearAlg';
 import MST from './Mst';
 import InitShell from './InitSh';
@@ -12,10 +13,11 @@ class App extends React.Component {
             <Router>
                 <div>
                     <Route exact path="/" component={Home} />
+                    <Route path="/charSet.md" component={CharSet} />
                     <Route path="/calculus.md" component={Calculus} />
                     <Route path="/linearAlg.md" component={LinearAlgMd} />
                     <Route path="/mst.md" component={MST} />
-					<Route path="/InitShell.md" component={InitShell} />
+                    <Route path="/InitShell.md" component={InitShell} />
                 </div>
             </Router>
         )
--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,11 @@ class Home extends React.Component {
     render() {
         return <div id="home">
             <a target="_blank" rel="noopener noreferrer" href="https://github.com/17hao">github</a>
-			<div>email: sqh1107@gmail.com</div>
-			<div>
+            <div>email: sqh1107@gmail.com</div>
+            <div>
+                <Link to="/charSet.md" style={{ color: 'black' }}>
+                    <div>字符编码</div>
+                </Link>
                 <Link to="/calculus.md" style={{ color: 'black' }}>
                     <div>微积分笔记</div>
                 </Link>
@@ -16,9 +19,9 @@ class Home extends React.Component {
                 <Link to="/mst.md" style={{ color: 'black' }}>
                     <div>最小生成树</div>
                 </Link>
-				<Link to="/InitShell.md" style={{ color: 'black' }}>
-					<div>迅速部署阿里云服务器</div>
-				</Link>
+                <Link to="/InitShell.md" style={{ color: 'black' }}>
+                    <div>迅速部署阿里云服务器</div>
+                </Link>
             </div>
         </div>;
     }
