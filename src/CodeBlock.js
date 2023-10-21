347b5bd3f2580eb3fece17e497bc3a7a0b511418
17hao <sqh1107@gmail.com>
2023-10-22 00:52:18 +0800

    fix dependencies

--- src/CodeBlock.js
+++ src/CodeBlock.js
@@ -3,12 +3,12 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 function CodeBlock(props) {
-  const { language, value } = props
-  return (
-    <SyntaxHighlighter language={language} style={docco}>
-      {value}
-    </SyntaxHighlighter>
-  );
+    const { language, value } = props
+    return (
+        <SyntaxHighlighter language={language} style={docco}>
+            {value}
+        </SyntaxHighlighter>
+    );
 }
 
 export default CodeBlock
--- src/MarkdownRender.js
+++ src/MarkdownRender.js
@@ -7,40 +7,40 @@ import gfm from 'remark-gfm';
 import React from 'react';
 
 class MarkdownRender extends React.Component {
-  constructor() {
-    super()
-    this.state = { markdown: '' }
-  }
+    constructor() {
+        super()
+        this.state = { markdown: '' }
+    }
 
-  componentDidMount() {
-    fetch(this.props.file).then(res => res.text()).then(text => this.setState({ markdown: text }));
-  }
+    componentDidMount() {
+        fetch(this.props.file).then(res => res.text()).then(text => this.setState({ markdown: text }));
+    }
 
-  render() {
-    const newProps = {
-      ...this.props,
-      plugins: [
-        RemarkMathPlugin,
-        gfm,
-      ],
-      renderers: {
-        ...{
-          text: text => text.value.replace(/:\w+:/gi, name => emoji.getUnicode(name)),
-          code: CodeBlock,
-        },
-        math: (props) =>
-          <MathJax.Node formula={props.value} />,
-        inlineMath: (props) =>
-          <MathJax.Node inline formula={props.value} />,
-      },
-      source: this.state.markdown,
-    };
-    return (
-      <MathJax.Provider input="tex">
-        <ReactMarkdown {...newProps} />
-      </MathJax.Provider>
-    );
-  }
+    render() {
+        const newProps = {
+            ...this.props,
+            plugins: [
+                RemarkMathPlugin,
+                gfm,
+            ],
+            renderers: {
+                ...{
+                    text: text => text.value.replace(/:\w+:/gi, name => emoji.getUnicode(name)),
+                    code: CodeBlock,
+                },
+                math: (props) =>
+                    <MathJax.Node formula={props.value} />,
+                inlineMath: (props) =>
+                    <MathJax.Node inline formula={props.value} />,
+            },
+            source: this.state.markdown,
+        };
+        return (
+            <MathJax.Provider input="tex">
+                <ReactMarkdown {...newProps} />
+            </MathJax.Provider>
+        );
+    }
 }
 
 export default MarkdownRender
