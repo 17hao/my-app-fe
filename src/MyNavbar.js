561f6d550be2e14753fbe77b81703d7135c8bf09
17hao <sqh1107@gmail.com>
2023-10-22 00:52:18 +0800

    fix dependencies

--- src/MyNavbar.js
+++ src/MyNavbar.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
 class MyNavbar extends React.Component {
-  render() {
-    return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">返回首页</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+    render() {
+        return (
+            <Navbar bg="light" expand="lg">
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">返回首页</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
 }
 
 export default MyNavbar;
