6dbde399dc9df5f8dd1438e4adea596ca7bf67f0
17hao <sqh1107@gmail.com>
2023-10-22 20:19:12 +0800

    fix 404

--- src/naive-navbar.js
+++ src/naive-navbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
@@ -9,13 +8,11 @@ class NaiveNavbar extends React.Component {
     render() {
         return (
             <Navbar bg="dark" data-bs-theme="dark">
-                <Container>
-                    <Nav>
-                        <Nav.Link href="/" className="custom-nav">Home</Nav.Link>
-                        <Nav.Link href="/articals" className="custom-nav">Articals</Nav.Link>
-                        <Nav.Link href="/aboutme" className="custom-nav">About</Nav.Link>
-                    </Nav>
-                </Container>
+                <Nav>
+                    <Nav.Link href="/" className="custom-nav">Home</Nav.Link>
+                    <Nav.Link href="/articals" className="custom-nav">Articals</Nav.Link>
+                    <Nav.Link href="/aboutme" className="custom-nav">About</Nav.Link>
+                </Nav>
             </Navbar>
         );
     }
