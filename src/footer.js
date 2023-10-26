594d55c71abb2dc540d9c9482870c1012854dca3
17hao <sqh1107@gmail.com>
2023-10-26 13:42:11 +0800

    add footer

---
+++ src/footer.js
@@ -0,0 +1,23 @@
+var style = {
+    backgroundColor: "#F8F8F8",
+    // borderTop: "1px solid #E7E7E7",
+    textAlign: "center",
+    padding: "10px",
+    // position: "fixed",
+    left: "0",
+    bottom: "0",
+    height: "20px",
+    // width: "100%",
+}
+
+function Footer() {
+    return (
+        <div>
+            <div style={style}>
+                <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">浙ICP备18016225号-2</a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
