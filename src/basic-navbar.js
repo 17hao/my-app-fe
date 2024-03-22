d168ad5d1d642ab4c096d0566362a5281ac70970
17hao <sqh1107@gmail.com>
2024-03-22 12:17:26 +0800

    update

--- src/basic-navbar.js
+++ src/basic-navbar.js
@@ -9,7 +9,7 @@ export default function BasicNavbar() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
--- src/footer.js
+++ src/footer.js
@@ -1,9 +1,9 @@
-var style = {
-    textAlign: "center",
-    height: "20px",
-}
-
 export default function Footer() {
+    const style = {
+        textAlign: "center",
+        height: "20px",
+    }
+
     return (
         <div style={style}>
             <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">浙ICP备18016225号-2</a>
