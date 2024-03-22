32b63913eea4a8e3f616b8bee682f7a760e69909
17hao <sqh1107@gmail.com>
2024-03-22 10:29:05 +0800

    fix footer

--- src/blog.js
+++ src/blog.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./blog.css"
+
+var style = {
+    margin: "10px",
+    color: "black"
+}
 
 export default function Blog(props) {
     const links = props.posts.map(post =>
-        <Link key={post.path} to={"/blog/" + post.path} style={{ color: "black" }} className="post-link">
+        <Link className="post-link" key={post.path} to={"/blog/" + post.path} style={style} >
             <div>{post.title}</div>
         </Link>
     )
--- src/footer.js
+++ src/footer.js
@@ -1,22 +1,12 @@
 var style = {
-    backgroundColor: "#F8F8F8",
-    // borderTop: "1px solid #E7E7E7",
     textAlign: "center",
-    padding: "10px 0",
-    // position: "absolute",
-    // position: "fixed",
-    // left: "0",
-    bottom: "0",
     height: "20px",
-    width: "100%",
 }
 
 export default function Footer() {
     return (
-        <div>
-            <div style={style}>
-                <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">浙ICP备18016225号-2</a>
-            </div>
+        <div style={style}>
+            <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">浙ICP备18016225号-2</a>
         </div>
     )
 }
