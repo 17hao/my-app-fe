2fac97829978ecf4d8a96b781798cd8ed14c58f2
17hao <sqh1107@gmail.com>
2024-08-31 11:48:45 +0800

    update about css

--- src/modules/about/about.js
+++ src/modules/about/about.js
@@ -7,29 +7,29 @@ export default function About() {
                 Experience
             </div>
             <div className="h3">
-                2019.07 - 2021.12&emsp;GrowingIO
-                <div>
-                    Software Engineer
+                &emsp;2019.07 - 2021.12&emsp;&emsp;GrowingIO
+                <div className="h4">
+                    &emsp;Software Engineer
                 </div>
             </div>
 
             <div className="h3">
-                2022.02 - 2024.04&emsp;ByteDance
-                <div>
-                    Software Engineer
+                &emsp;2022.02 - 2024.04&emsp;&emsp;ByteDance
+                <div className="h4">
+                    &emsp;Software Engineer
                 </div>
             </div>
             <div className="h3">
-                2024.07 - Present&emsp;Alibaba Group
-                <div>
-                    Software Engineer
+                &emsp;2024.07 - Present&emsp;&emsp;Alibaba Group
+                <div className="h4">
+                    &emsp;Software Engineer
                 </div>
             </div>
             <div className="h2">
                 Contact info
             </div>
             <div className="h3">
-                sqh1107@gmail.com
+                &emsp;sqh1107@gmail.com
             </div>
         </div>
     )
