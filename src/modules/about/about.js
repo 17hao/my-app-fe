a80d33a6d50b72f631f45d4501fea5513a8c51a2
17hao <sqh1107@gmail.com>
2024-08-31 10:34:46 +0800

    update about me

--- src/modules/about/about.js
+++ src/modules/about/about.js
@@ -1,10 +1,36 @@
+import "modules/about/about.css"
+
 export default function About() {
     return (
-        <div style={{ marginLeft: "3%" }}>
-            <p>
-                github: <a target="_blank" rel="noopener noreferrer" href="https://github.com/17hao">https://github.com/17hao</a>
-            </p>
-            <p>email: sqh1107@gmail.com</p>
+        <div className="h1">
+            <div className="h2">
+                Experience
+            </div>
+            <div className="h3">
+                2019.07 - 2021.12&emsp;GrowingIO
+                <div>
+                    Software Engineer
+                </div>
+            </div>
+
+            <div className="h3">
+                2022.02 - 2024.04&emsp;ByteDance
+                <div>
+                    Software Engineer
+                </div>
+            </div>
+            <div className="h3">
+                2024.07 - Present&emsp;Alibaba Group
+                <div>
+                    Software Engineer
+                </div>
+            </div>
+            <div className="h2">
+                Contact info
+            </div>
+            <div className="h3">
+                sqh1107@gmail.com
+            </div>
         </div>
     )
 }
