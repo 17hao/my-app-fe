b6fa4c7b905e0fbc6dcbab1727044f4a4ae867d4
17hao <sqh1107@gmail.com>
2025-02-04 23:04:29 +0800

    support ipv6

--- start-my-app-fe.sh
+++ start-my-app-fe.sh
@@ -8,5 +8,4 @@ docker rm my-app-fe
 
 docker rmi $(docker images -f "dangling=true" -q)
 
-# docker network create --driver=bridge --subnet=192.168.100.0/24 nginx
-docker run --name my-app-fe -d --network=nginx -p 3000:3000 17hao/my-app-fe
+docker run --name my-app-fe -d -p 18080:18080 17hao/my-app-fe
