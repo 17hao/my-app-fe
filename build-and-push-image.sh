0bc795a02daf7130eb29d3b7f272d27d3333cb62
17hao <sqh1107@gmail.com>
2026-01-15 23:20:05 +0800

    Add ci/cd script

---
+++ build-and-push-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export https_proxy=http://127.0.0.1:8889
+
+docker stop my-app-fe
+
+docker rm my-app-fe
+
+docker build --network=host -t 17hao/my-app-fe .
+
+docker rmi $(docker images -qa -f 'dangling=true')
+
+docker push 17hao/my-app-fe:latest
